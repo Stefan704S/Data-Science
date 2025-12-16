c00a06bf84181ce116ea60f8d0bc157ff1b73774
Stefan704 <stefan.stevanovic@unine.ch>
2025-12-16 02:46:15 +0100

    Improving and correcting the code

--- main.py
+++ main.py
@@ -13,14 +13,20 @@ print(data.describe())
 
 
 # ----------------------------------------------------------------------------------------------
-# 5)   Plot WCSS and choose k*
+#Results for the hypothesis 0
 
 from src.models import compute_elbow_scores
 from src.evaluation import plot_elbow_curve
 from src.data_loader import standardize
+from src.models import kmeans
+from src.models import anova
+from src.models import silhouette
 
-x_full_scaled = standardize(data)
 
+# 5)   Plot WCSS and choose k*
+
+vars_h0 = ["3Mth", "10Yd", "Inf", "Unmp", "CHF", "GDP", "SMI"]
+x_full_scaled = standardize(data[vars_h0])
 print("\n=== Elbow (full variables) ===")
 k_range = range(1, 11)
 
@@ -33,20 +39,58 @@ plot_elbow_curve(
     save_path="results/elbow_full.png",
 )
 
-
-#-------------------------------------------------------------------------------------------------------------------------------
-#Results for the hypothesis 0
-
-from src.models import kmeans
-from src.models import anova
-from src.models import silhouette
+# 6 Run Anova and Silhouette test
 
 # K-means with k = 3 and all the variables
 labels_0, kmeans_0 = kmeans(x_full_scaled, n_clusters=3)
 data["Cluster_0"] = labels_0
 
+# Anova test
 variables_0 = ["3Mth", "10Yd", "Inf", "Unmp", "CHF", "GDP", "SMI"]
 anova(data, cluster_col="Cluster_0", variables=variables_0)
 
+# Silhouette test
 sil_0 = silhouette(x_full_scaled, labels_0)
 print(sil_0)
+
+
+#-------------------------------------------------------------------------------------------------------------------------------
+#Results for the hypothesis 1
+
+from src.models import compute_elbow_scores
+from src.evaluation import plot_elbow_curve
+from src.data_loader import standardize
+from src.models import kmeans
+from src.models import anova
+from src.models import silhouette
+
+# 7)   Plot WCSS and choose k*
+
+vars_h1 = ["3Mth", "10Yd", "Inf", "Unmp", "CHF"]
+x_new_scaled = standardize(data[vars_h1])
+print("\n=== Elbow (full variables) ===")
+k_range = range(1, 11)
+
+score_h1 = compute_elbow_scores(x_new_scaled)
+
+plot_elbow_curve(
+    k_values=k_range,
+    scores=score_h1,
+    title="Elbow Method for Optimal k (full variables)",
+    save_path="results/elbow_new.png",
+)
+
+# 8 Run Anova and Silhouette test
+
+# K-means with k = 3 and all the variables
+labels_1, kmeans_1 = kmeans(x_new_scaled, n_clusters=3)
+data["Cluster_1"] = labels_1
+
+# Anova test
+variables_1 = ["3Mth", "10Yd", "Inf", "Unmp", "CHF"]
+anova(data, cluster_col="Cluster_1", variables=variables_1)
+
+# Silhouette test
+sil_1 = silhouette(x_new_scaled, labels_1)
+print(sil_1)
+
--- src/data_loader.py
+++ src/data_loader.py
@@ -14,11 +14,8 @@ def data_load(path="data/raw/data.slsx"):
 from sklearn.preprocessing import StandardScaler
 
 
-def standardize(data: pd.DataFrame):
+def standardize(x: pd.DataFrame):
     # Standardization of variables to test the first hypothesis. In this part of the code, we select the data we want to standardize, create a new matrix, and then add the standardized data from the initial database to it.
 
-    # Hypotesis 0
-    x_full = data[["3Mth", "10Yd", "Inf", "Unmp", "CHF", "GDP", "SMI"]]
-    scaler_full = StandardScaler()
-    x_full_scaled = scaler_full.fit_transform(x_full)
-    return x_full_scaled
+    scaler = StandardScaler()
+    return scaler.fit_transform(x)
